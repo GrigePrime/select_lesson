a35086d4bf262f0335a9db9558ba363413df6024
rampageben <adam23ben1012@gmail.com>
2023-11-13 19:04:27 +0800

    finish add class

--- templates_c/2dce9797727ecf1db7fbcf2c46c0abd89a0faae7_0.file.showaddclass.html.php
+++ templates_c/2dce9797727ecf1db7fbcf2c46c0abd89a0faae7_0.file.showaddclass.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2023-11-12 18:04:43
+/* Smarty version 3.1.34-dev-7, created on 2023-11-13 10:00:00
   from 'D:\lesson\select_lesson\templates\showaddclass.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_655113bb6f4ec9_65447957',
+  'unifunc' => 'content_6551f3a0941358_12203116',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2dce9797727ecf1db7fbcf2c46c0abd89a0faae7' => 
     array (
       0 => 'D:\\lesson\\select_lesson\\templates\\showaddclass.html',
-      1 => 1699812279,
+      1 => 1699869598,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_655113bb6f4ec9_65447957 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6551f3a0941358_12203116 (Smarty_Internal_Template $_smarty_tpl) {
 ?><table class="table">
     <thead>
     <tr>
@@ -61,6 +61,21 @@ foreach ($_from as $_smarty_tpl->tpl_vars['class']->value) {
 </td>
         <td><?php echo $_smarty_tpl->tpl_vars['class']->value['course_people'];?>
 </td>
+        <form action="Addclass.php" class="form-inline" method="get">
+            <input id="chose_class" name="op" type="hidden" value="chose_class">
+            <input class="form-control" id="class_id" name="class_id" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['class_id']->value;?>
+">
+            <input  id ="chose_id" name="chose_id" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['class']->value['course_id'];?>
+">
+            <td><?php if ($_smarty_tpl->tpl_vars['class']->value['already'] == "true") {?>
+                已選
+                <?php } elseif ($_smarty_tpl->tpl_vars['class']->value['already'] == "false") {?>
+                <button class="btn btn-danger"  type="submit" >加選</button>
+                <?php } else { ?>
+                已滿
+                <?php }?>
+            </td>
+        </form>
     </tr>
     </tbody>
     <?php
--- templates_c/ca7bf6c1b8c1285d36680af9b37e6f20c55acb33_0.file.addclass.html.php
+++ templates_c/ca7bf6c1b8c1285d36680af9b37e6f20c55acb33_0.file.addclass.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2023-11-12 18:15:54
+/* Smarty version 3.1.34-dev-7, created on 2023-11-13 11:01:38
   from 'D:\lesson\select_lesson\templates\addclass.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_6551165acd7d38_58627448',
+  'unifunc' => 'content_655202128359d2_77525651',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ca7bf6c1b8c1285d36680af9b37e6f20c55acb33' => 
     array (
       0 => 'D:\\lesson\\select_lesson\\templates\\addclass.html',
-      1 => 1699812951,
+      1 => 1699873172,
       2 => 'file',
     ),
   ),
@@ -20,18 +20,24 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6551165acd7d38_58627448 (Smarty_Internal_Template $_smarty_tpl) {
+function content_655202128359d2_77525651 (Smarty_Internal_Template $_smarty_tpl) {
 ?><h2>加選</h2>
 <br>
 <form action="Addclass.php" class="form-inline" method="get" style="padding-left: 70px;">
+    <input id="addclass" name="op" type="hidden" value="addclass">
     <input class="form-control" id="class_id" name="class_id" placeholder="請輸入課程代號" style="margin-top: 5px; margin-bottom: 3px;"
            type="text">
-    <button id="add_search"  style="width: 30px;height: 30px;" type="submit">
-        <img src="templates/web_image/search.png" alt="search" width="20px">
+    <button id="add_search" style="width: 30px;height: 30px;" type="submit">
+        <img alt="search" src="templates/web_image/search.png" width="20px">
     </button>
-    <?php if (isset($_smarty_tpl->tpl_vars['msg']->value)) {?>
+    <?php if (isset($_smarty_tpl->tpl_vars['msgdanger']->value)) {?>
     <br>
-    <b class="text-danger">●<?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+    <b class="text-danger">●<?php echo $_smarty_tpl->tpl_vars['msgdanger']->value;?>
+</b>
+    <?php }?>
+    <?php if (isset($_smarty_tpl->tpl_vars['msgsuccess']->value)) {?>
+    <br>
+    <b class="text-success">●<?php echo $_smarty_tpl->tpl_vars['msgsuccess']->value;?>
 </b>
     <?php }?>
 </form>
