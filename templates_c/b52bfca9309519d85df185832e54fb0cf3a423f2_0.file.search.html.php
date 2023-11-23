04e491f573d34bc5372c6ae86dfbf8ba934958e9
Boewi155 <2358309@gmail.com>
2023-11-24 00:13:36 +0800

    add button

--- templates_c/b52bfca9309519d85df185832e54fb0cf3a423f2_0.file.search.html.php
+++ templates_c/b52bfca9309519d85df185832e54fb0cf3a423f2_0.file.search.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2023-11-23 15:59:29
+/* Smarty version 3.1.34-dev-7, created on 2023-11-23 16:12:52
   from 'C:\Users\ASUS\WebstormProjects\select_lesson\templates\search.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_655f76e1329b04_54894735',
+  'unifunc' => 'content_655f7a04245954_87939342',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b52bfca9309519d85df185832e54fb0cf3a423f2' => 
     array (
       0 => 'C:\\Users\\ASUS\\WebstormProjects\\select_lesson\\templates\\search.html',
-      1 => 1700755168,
+      1 => 1700755971,
       2 => 'file',
     ),
   ),
@@ -20,22 +20,25 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_655f76e1329b04_54894735 (Smarty_Internal_Template $_smarty_tpl) {
+function content_655f7a04245954_87939342 (Smarty_Internal_Template $_smarty_tpl) {
 ?><br>
 <form action="Search.php" class="form-inline" method="get" style="padding-left: 70px;">
     <input id="search" name="op" type="hidden" value="search">
     <input class="form-control" id="class_id" name="class_id" placeholder="請輸入課程代號" style="margin-top: 5px; margin-bottom: 3px;"
            type="text">
-<!--    <button id="searchIDbutton" style="width: 30px;height: 30px;" type="submit">-->
-<!--        <img alt="search" src="templates/web_image/search.png" width="20px">-->
-<!--    </button>-->
+    <button id="searchIDbutton" style="width: 30px;height: 30px;" type="submit">
+        <img alt="search" src="templates/web_image/search.png" width="20px">
+    </button>
     </br>
     <input class="form-control" id="class_name" name="class_name" placeholder="請輸入課程名稱" style="margin-top: 5px; margin-bottom: 3px;"
            type="text">
+    <button id="searchnamebutton" style="width: 30px;height: 30px;" type="submit">
+        <img alt="search" src="templates/web_image/search.png" width="20px">
+    </button>
     </br>
     <input class="form-control" id="class_teacher" name="class_teacher" placeholder="請輸入課程教授" style="margin-top: 5px; margin-bottom: 3px;"
            type="text">
-    <button id="searchbutton" style="width: 30px;height: 30px;" type="submit">
+    <button id="searchteacherbutton" style="width: 30px;height: 30px;" type="submit">
         <img alt="search" src="templates/web_image/search.png" width="20px">
     </button>
     <?php if (isset($_smarty_tpl->tpl_vars['msgdanger']->value)) {?>
